211621c376cf7f8082518bd9e2db198dd14236d8
Eran <iamzealotwang@126.com>
2019-10-15 12:08:34 +0800

    Site updated: 2019-10-15 12:08:34

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue Sep 24 2019 15:59:53 GMT+0800 (中国标准时间)
+// build time:Tue Oct 15 2019 12:08:29 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
